e392521e20f4ee47f4454448bdddc1611f9ed529
XW045486 <Alina.Wang@Cerner.com>
2016-05-10 17:01:51 -0500

    some changes

--- src/main/webapp/js/globalVariables.js
+++ src/main/webapp/js/globalVariables.js
@@ -1,1 +1,5 @@
 var fbAuthorizationCodeFlowURL = "https://www.fitbit.com/oauth2/authorize?response_type=code&client_id=227KTY&redirect_uri=http%3A%2F%2Flocalhost%3A8080%2FStepRace%2Fhtml%2Findex.html&scope=activity%20nutrition%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight";
+var fbRedirectURL = "http://localhost:8080/StepRace/html/index.html";
+var fbAccessTokenURL = "https://api.fitbit.com/oauth2/token";
+var fbLeaderBoardJSONURL = "https://api.fitbit.com/1/user/-/friends/leaderboard.json";
+var fbActivityGoalJSONURL = "https://api.fitbit.com/1/user/-/activities/goals/daily.json";
--- src/main/webapp/js/indexController.js
+++ src/main/webapp/js/indexController.js
@@ -1,26 +1,47 @@
 (function() {
 	var indexApp = angular.module('indexApp', []);
-	
-	indexApp.controller('accessToken', function($scope, $http) {
+	indexApp.value('accessTokenValue','');
+
+	indexApp.controller('accessToken', ['accessTokenValue', function(accessTokenValue) {
+		var accessToken = null;
 		var returnURL = window.location.href;
 		var codeVarChar = returnURL.indexOf('code=') + 5;
-		var successVarChar = returnURL.indexOf('#success');
+		var successVarChar = returnURL.indexOf('#');
 		var auCode = returnURL.substring(codeVarChar, successVarChar);
 		var requestPayload = {
 			'grant_type' : 'authorization_code',
 			'client_id' : '227KTY',
-			'redirect_uri' : 'http://localhost:8080/LearnAngular/pages/index.html',
+			'redirect_uri' : fbRedirectURL,
 			'code' : auCode
-		}
-	    $http({
-	        method : 'POST',
-	        url : 'https://api.fitbit.com/oauth2/token'
-	    }).then(function success(response) {
-	        alert(response.data);
-	    }, function myError(response) {
-	        alert(response.statusText);
-	    });
-	});
+		};
+		$.ajax({
+			'url' : fbAccessTokenURL,
+			'type' : 'POST',
+			'Content-Type' : 'application/x-www-form-urlencoded',
+			'dataType' : 'json',
+			'headers' : {
+				'Authorization' : 'Basic '
+						+ 'MjI3S1RZOjQyYWUyZGVmMDZjZjc2MTQyNzM2NGI4MTc2ZjI2ZjI4'
+			},
+			'data' : requestPayload,
+			'success' : function(result) {
+				// Process success actions
+				accessTokenValue = result.access_token;
+				dataLoadWithUrl(accessTokenValue, fbLeaderBoardJSONURL);
+				dataLoadWithUrl(accessTokenValue, fbActivityGoalJSONURL);
+			},
+			'error' : function(XMLHttpRequest, textStatus, errorThrown) {
+				// Process error actions
+				alert('Error: ' + errorThrown);
+				console
+						.log(XMLHttpRequest.status + ' '
+								+ XMLHttpRequest.statusText);
+			}
+		});
+		return {
+			accessToken: accessTokenValue
+		};
+	}]);
 
 	indexApp.directive('navHead', function() {
 		return {
@@ -42,4 +63,61 @@
 			templateUrl : 'indexPage.html'
 		};
 	});
+	
+	function dataLoadWithUrl(accessTokenValue, url){
+		$.ajax({
+			'url' : url,
+			'type' : 'GET',
+			'content-Type' : 'x-www-form-urlencoded',
+			'dataType' : 'json',
+			'headers' : {
+				'Authorization' : 'Bearer ' + accessTokenValue
+			},
+			'success' : function(result) {
+				// Process success actions
+				var resultJSON = JSON.stringify(result);
+				switch(url){
+				case fbLeaderBoardJSONURL:
+					loadFriendsList(resultJSON);
+					break;
+				case fbActivityGoalJSONURL:
+					
+				}
+				
+			},
+			'error' : function(XMLHttpRequest, textStatus, errorThrown) {
+				// Process error actions
+				alert('Error: ' + errorThrown);
+				console
+						.log(XMLHttpRequest.status + ' '
+								+ XMLHttpRequest.statusText);
+				return false;
+			}
+		});
+	}
+	
+	function loadFriendsList(friendsJSON){
+		var friendsTable = $('#friendsTable').DataTable({
+			ordering: false,
+			columns: [
+			          	{	title: "Rank"},
+			          	{
+			          		title: "Profile",
+			          		render: function(data, type, row) {
+			    		        return '<img src="'+data+'" class="img-circle" width=50 height=50/>';
+			    		    }},
+			            {	title: "Friend"},
+			            {	title: "Steps"}
+			        ]
+		});
+		var friendsList = JSON.parse(friendsJSON);
+		for(var i = 0; i < friendsList.friends.length; i++){
+			friendsTable.row.add([
+			                      friendsList.friends[i].rank.steps,
+			                      friendsList.friends[i].user.avatar,
+			                      friendsList.friends[i].user.displayName, 
+			                      friendsList.friends[i].average.steps
+			                      ]).draw(false);
+		}
+	}
 })();
